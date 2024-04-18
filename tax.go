95df4d76afa34a139e23b96e4eeff3c7ff3f69a1
Ruchida Pithaksiripan <im@fai.in.th>
2024-04-19 06:24:54 +0700

    Add calculate tax function

--- tax.go
+++ tax.go
@@ -6,6 +6,35 @@ type Allowance struct {
 }
 
 func CalculateTax(totalIncome, wht float64, allowances []Allowance) (float64, error) {
-	// TODO: Implement the tax calculation logic here
-	return 0, nil
+	totalAllowance := 60000.0
+	grossIncome := totalIncome - totalAllowance
+	totalTax := 0.0
+	if grossIncome <= 150000 {
+		return totalTax, nil
+	}
+	grossIncome -= 150000
+
+	if grossIncome <= 350000 {
+		totalTax += grossIncome * 0.1
+		return totalTax, nil
+	}
+	totalTax += 350000 * 0.1
+	grossIncome -= 350000
+
+	if grossIncome <= 500000 {
+		totalTax += grossIncome * 0.15
+		return totalTax, nil
+	}
+	totalTax += 500000 * 0.15
+	grossIncome -= 500000
+
+	if grossIncome <= 1000000 {
+		totalTax += grossIncome * 0.2
+		return totalTax, nil
+	}
+	totalTax += 1000000 * 0.2
+	grossIncome -= 1000000
+
+	totalTax += grossIncome * 0.35
+	return totalTax, nil
 }
