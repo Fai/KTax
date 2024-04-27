1c031b92651f86b4b1fb5544c1e707c4881b7259
Ruchida Pithaksiripan <im@fai.in.th>
2024-04-27 15:31:49 +0700

    Add graceful shutdown

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-// @title tax API
-// @version 1.0
-// @description This is a k-tax API
-// @host localhost:8080
 func main() {
 	port := os.Getenv("PORT")
-
 	e := echo.New()
-
+	e.Logger.SetLevel(log.INFO)
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
+		return c.JSON(http.StatusOK, "Hello, Go Bootcamp!")
 	})
 
-	e.Logger.Fatal(e.Start(":" + port))
+	go func() {
+		if err := e.Start(":" + port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("\nshutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
