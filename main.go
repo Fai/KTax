0b7a5208f7eaf15aad1ef764abac7c31f1b12412
Ruchida Pithaksiripan <im@fai.in.th>
2024-04-27 10:47:09 +0700

    Update PORT with env

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"os"
 )
 
+// @title tax API
+// @version 1.0
+// @description This is a k-tax API
+// @host localhost:8080
 func main() {
+	port := os.Getenv("PORT")
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
